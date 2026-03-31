18f60dcb2cd786b7f2dab40dd4d152ea2b53809e
padmagandla <padmalaxmirajamg@gmail.com>
2026-03-31 17:14:58 +0530

    Commited

---
+++ src/main/java/com/example/demo/Myspring2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myspring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myspring2Application.class, args);
+	}
+
+}
